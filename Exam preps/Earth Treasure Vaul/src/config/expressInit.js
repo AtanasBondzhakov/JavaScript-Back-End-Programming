ce7e59f93056e5b9a9b4ced1a7c7b894b745dbfa
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-01-09 23:07:11 +0200

    Install jsonwebtoken and cookie-parser, Add jwt promise

--- Exam preps/Earth Treasure Vaul/src/config/expressInit.js
+++ Exam preps/Earth Treasure Vaul/src/config/expressInit.js
@@ -1,9 +1,11 @@
 import express, { urlencoded } from 'express';
+import cookieParser from 'cookie-parser';
 
 import routes from '../routes.js';
 
 export default function expressInit(app) {
     app.use('/static', express.static('src/public'));
     app.use(urlencoded({ extended: false }));
+    app.use(cookieParser());
     app.use(routes);
 }
