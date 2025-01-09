4b27c09ca1253a828fa64cd621a1b75f3d5e7e33
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-01-09 21:50:20 +0200

    Configure Home Controller and add layout

--- Exam preps/Earth Treasure Vaul/src/config/expressInit.js
+++ Exam preps/Earth Treasure Vaul/src/config/expressInit.js
@@ -1,6 +1,9 @@
 import express, { urlencoded } from 'express';
 
+import routes from '../routes.js';
+
 export default function expressInit(app) {
-    app.use('/styles', express.static('src/public'));
-    app.use(urlencoded({extended: false}));
+    app.use('/static', express.static('src/public'));
+    app.use(urlencoded({ extended: false }));
+    app.use(routes);
 }
--- Exam preps/Earth Treasure Vaul/src/index.js
+++ Exam preps/Earth Treasure Vaul/src/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 
 import expressInit from './config/expressInit.js';
+import handlebarsInit from './config/handlebarsInit.js';
 
 const app = express();
 
+
+
 expressInit(app);
+handlebarsInit(app);
 
 app.listen(3000, () => console.log('Server is listening on http://localhost:3000...'));
