eac009a077e625ddad89143f259119052ae8aab5
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-01-09 22:01:33 +0200

    Install Mongoose, connect to local DB and add User model

---
+++ Exam preps/Earth Treasure Vaul/src/config/mongooseInit.js
@@ -0,0 +1,12 @@
+import { connect } from "mongoose";
+
+const dbUrl = 'mongodb://127.0.0.1:27017';
+
+export default async function mongooseInit() {
+    try {
+        await connect(dbUrl, { dbName: 'earth treasures' });
+        console.log('Successfully connected to DB!');
+    } catch (err) {
+        console.log('Cannot connect to DB!' + err.message);
+    }
+}
--- Exam preps/Earth Treasure Vaul/src/index.js
+++ Exam preps/Earth Treasure Vaul/src/index.js
@@ -2,11 +2,12 @@ import express from 'express';
 
 import expressInit from './config/expressInit.js';
 import handlebarsInit from './config/handlebarsInit.js';
+import mongooseInit from './config/mongooseInit.js';
 
 const app = express();
 
 
-
+mongooseInit();
 expressInit(app);
 handlebarsInit(app);
 
