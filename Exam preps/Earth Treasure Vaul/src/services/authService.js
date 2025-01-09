3a48c1857b446832f0c60d62b0fc79339c888f98
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-01-09 22:56:19 +0200

    Install bcrypt library, hash password on register, check for unique user

--- Exam preps/Earth Treasure Vaul/src/services/authService.js
+++ Exam preps/Earth Treasure Vaul/src/services/authService.js
@@ -2,8 +2,16 @@ import User from "../models/User.js"
 
 export const authService = {
     async register(email, password, rePassword) {
-        console.log(email);
-        
+        if (password !== rePassword) {
+            throw new Error('Passwords don\'t match!');
+        }
+
+        const user = await User.findOne({ $or: [{ username }, { email }] });
+
+        if(user) {
+            throw new Error('User already exist!');
+        }
+
         return await User.create({ email, password });
     }
 }
