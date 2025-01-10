58e4f0830e1dbed51b9d5daabb1e62aa865a7930
atanasbondzhakov <atanas.b@yahoo.com>
2025-01-10 11:51:16 +0200

    Add Dashboard page and partials

--- Exam preps/Earth Treasure Vaul/src/controllers/stoneController.js
+++ Exam preps/Earth Treasure Vaul/src/controllers/stoneController.js
@@ -12,18 +12,30 @@ stoneController.get('/create', isAuth, (req, res) => {
 stoneController.post('/create', async (req, res) => {
     const stoneData = req.body;
     const ownerId = req.user._id;
-    // console.log(owner);
-    
 
     try {
         await stoneService.create(stoneData, ownerId);
 
-        res.render('/dashboard', { title: 'Dashboard Page' });
+        res.redirect('/stones/dashboard')
     } catch (err) {
         //TODO error
-        res.render('/create', {title: 'Create Page', stone: stoneData})
+        res.render('stones/create', { title: 'Create Page', stone: stoneData })
+    }
+});
+
+stoneController.get('/dashboard', async (req, res) => {
+    try {
+        const stones = await stoneService.getAll().lean();
+        
+        res.render('stones/dashboard', { title: 'Dashboard Page', stones });
+    } catch (err) {
+        //TODO error
+
+        res.render('stones/dashboard', { title: 'Dashboard Page' });
     }
 
-})
+});
+
+
 
 export default stoneController;
--- Exam preps/Earth Treasure Vaul/src/services/stoneService.js
+++ Exam preps/Earth Treasure Vaul/src/services/stoneService.js
@@ -3,5 +3,8 @@ import Stone from "../models/Stone.js";
 export const stoneService = {
     create(stoneData, ownerId) {
         return Stone.create({...stoneData, owner: ownerId});
+    },
+    getAll() {
+        return Stone.find();
     }
 }
