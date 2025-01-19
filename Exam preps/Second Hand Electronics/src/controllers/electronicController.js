e7c7ae0702d2c66f31dfa42ac29b3d60adb99cc6
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-01-19 15:41:23 +0200

    Add catalog page

--- Exam preps/Second Hand Electronics/src/controllers/electronicController.js
+++ Exam preps/Second Hand Electronics/src/controllers/electronicController.js
@@ -8,16 +8,26 @@ electronicController.get('/create', (req, res) => {
     res.render('electronics/create', { title: 'Create Page' });
 });
 
-electronicController.post('/create', (req, res) => {
+electronicController.post('/create', async (req, res) => {
     const electronicData = req.body;
     const ownerId = req.user?._id;
 
     try {
-        electronicService.create(electronicData, ownerId);
+        await electronicService.create(electronicData, ownerId);
 
         res.redirect('/electronics/catalog')
     } catch (err) {
         res.render('electronics/create', { title: 'Create Page', error: getErrorMessage(err) });
+    }
+});
+
+electronicController.get('/catalog', async (req, res) => {
+    try {
+        const electronics = await electronicService.getAll().lean();
+
+        res.render('electronics/catalog', { title: 'Catalog Page', electronics });
+    } catch (err) {
+        res.render('electronics/catalog', { title: 'Catalog Page', error: getErrorMessage(err) });
     }
 });
 
--- Exam preps/Second Hand Electronics/src/services/electronicService.js
+++ Exam preps/Second Hand Electronics/src/services/electronicService.js
@@ -3,5 +3,8 @@ import Electronic from "../models/Electronic.js"
 export const electronicService = {
     create(electronicData, ownerId) {
         return Electronic.create({...electronicData, owner: ownerId});
+    },
+    getAll() {
+        return Electronic.find();
     }
 }
