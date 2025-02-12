3cc5010a38d1385aa4f61a9839c104a144a20c1d
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-02-12 21:36:06 +0200

    Fix get all posts

--- Exam preps/Wizard Creatures Wiki/src/controllers/creaturesController.js
+++ Exam preps/Wizard Creatures Wiki/src/controllers/creaturesController.js
@@ -1,23 +1,17 @@
 import { Router } from "express";
 import creatureService from "../services/creatureService.js";
+import { getErrorMessage } from "../utils/errorUtils.js";
 
 const creatureController = Router();
 
-creatureController.get('/all-posts', (req, res) => {
-    res.render('creatures/all-posts', { title: 'Catalog Page' });
-});
-
-creatureController.post('/all-posts', async (req, res) => {
-
+creatureController.get('/all-posts', async (req, res) => {
     try {
-        const creatures = await creatureService.getAll();
+        const creatures = await creatureService.getAll().lean();
 
-        res.redirect('/creatures/all-posts', creatures);
+        res.render('creatures/all-posts', { title: 'Catalog Page', creatures });
     } catch (err) {
-
+        res.render('creatures/all-posts', { title: 'Catalog Page', error: getErrorMessage(err) });
     }
-
-
 });
 
 export default creatureController;
