d7e974e9b0020d99e53ad9797a6fce3a66de3796
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-02-12 20:48:56 +0200

    Update database name and port

--- Exam preps/Wizard Creatures Wiki/src/config/mongooseInit.js
+++ Exam preps/Wizard Creatures Wiki/src/config/mongooseInit.js
@@ -1,11 +1,11 @@
 import { connect } from "mongoose";
 
-const dbUrl = 'mongodb://localhost:27017';
+const dbUrl = 'mongodb://127.0.0.1:27017';
 
 export default async function mongooseInit() {
     try {
         // TODO change dbName
-        await connect(dbUrl, {dbName: 'volcanoes'});
+        await connect(dbUrl, {dbName: 'wizardCreatures'});
         console.log('Successfully connected to DB!');
     } catch (error) {
         console.log('Cannot connect to DB!' + error.message);
