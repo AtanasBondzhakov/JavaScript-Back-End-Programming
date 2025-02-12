d66eefc600f417b8701c11c1849a83b352c1d1ed
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-02-12 21:47:40 +0200

    Add create page

--- Exam preps/Wizard Creatures Wiki/src/controllers/creaturesController.js
+++ Exam preps/Wizard Creatures Wiki/src/controllers/creaturesController.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import creatureService from "../services/creatureService.js";
 import { getErrorMessage } from "../utils/errorUtils.js";
+import { isAuth } from "../middlewares/authMIddleware.js";
 
 const creatureController = Router();
 
@@ -11,6 +12,23 @@ creatureController.get('/all-posts', async (req, res) => {
         res.render('creatures/all-posts', { title: 'Catalog Page', creatures });
     } catch (err) {
         res.render('creatures/all-posts', { title: 'Catalog Page', error: getErrorMessage(err) });
+    }
+});
+
+creatureController.get('/create', isAuth, (req, res) => {
+    res.render('creatures/create', { title: 'Create Page' });
+});
+
+creatureController.post('/create', isAuth, async (req, res) => {
+    const creatureData = req.body;
+    const ownerId = req.user?._id;
+
+    try {
+        await creatureService.create(creatureData, ownerId);
+
+        res.redirect('/creatures/all-posts');
+    } catch (err) {
+        res.render('creatures/create', { title: 'Create Page', error: getErrorMessage(err), creature: creatureData });
     }
 });
 
--- Exam preps/Wizard Creatures Wiki/src/services/creatureService.js
+++ Exam preps/Wizard Creatures Wiki/src/services/creatureService.js
@@ -2,6 +2,11 @@ import Creature from "../models/Creatures.js";
 
 const getAll = () => Creature.find();
 
+const create = (creatureData, ownerId) => {
+    return Creature.create({ ...creatureData, owner: ownerId });
+}
+
 export default {
-    getAll
+    getAll,
+    create
 }
