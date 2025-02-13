93a8c1a144fca611d77631090445f8b0ee95d6cd
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-02-13 18:46:36 +0200

    Add validations

--- Exam preps/Wizard Creatures Wiki/src/models/Creatures.js
+++ Exam preps/Wizard Creatures Wiki/src/models/Creatures.js
@@ -3,27 +3,36 @@ import { Schema, Types, model } from "mongoose";
 const creatureSchema = new Schema({
     name: {
         type: String,
-        required: [true, "Name is required"]
+        required: [true, "Name is required"],
+        minLength: [2, 'Name should be at least 2 characters long']
+
     },
     species: {
         type: String,
-        required: [true, "Species is required"]
+        required: [true, "Species is required"],
+        minLength: [3, 'Species should be at least 3 characters long']
+
     },
     skinColor: {
         type: String,
-        required: [true, "Skin color is required"]
+        required: [true, "Skin color is required"],
+        minLength: [3, 'Skin color should be at least 3 characters long']    
     },
     eyeColor: {
         type: String,
-        required: [true, "Eye color is required"]
+        required: [true, "Eye color is required"],
+        minLength: [3, 'Eye color should be at least 3 characters long']
     },
     image: {
         type: String,
-        required: [true, "Image is required"]
+        required: [true, "Image is required"],
+        validate: [/^https?:\/\/.+/, 'Image should start with http:// or https://']
     },
     description: {
         type: String,
-        required: [true, "Description is required"]
+        required: [true, "Description is required"],
+        minLength: [5, 'Description should be at least 5 characters long'],
+        maxLength: [500, 'Description cannot be at more than 500 characters long']
     },
     votes: [{
         type: Types.ObjectId,
